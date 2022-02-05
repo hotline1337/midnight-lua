6afec35e4662ca5282717744ca8d8dc2d93f5f61
hotline1337 <32012110+hotline1337@users.noreply.github.com>
2022-02-05 16:35:38 +0100

    Create max_nightclub_popularity.lua

---
+++ max_nightclub_popularity.lua
@@ -0,0 +1,17 @@
+-- made by hotline / github.com/hotline1337
+
+require("lib/natives")
+
+local function handle_mp_index(index)
+    if index == 0 then 
+        return "MP0_" 
+    else
+        return "MP1_"
+    end
+end
+
+system.fiber(function()
+    local player_index = script_global:new(1574907):get_int64()
+    STATS.STAT_SET_INT(string.smart_joaat(handle_mp_index(player_index) .. "CLUB_POPULARITY"), 1000)
+    utils.notify("Nightclub Popularity", "Successfully changed your club popularity to max", 28, 1)
+end)
